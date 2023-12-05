e12392612dc3f1a88025fbaf5a4d825b16cd4502
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2023-12-04 21:22:05 -0700

    initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("SPTMod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SPTMod")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("{ProjectGUID}")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SPTModPlugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace {AuthorName}.SPTMod {
+	[BepInPlugin("{AuthorDomain}.sptmod", "SPTMod", "1.0.0")]
+	public class SPTModPlugin : BaseUnityPlugin {
+		private void Awake() {
+			this.Logger.LogInfo("Initialized");
+		}
+
+		private void OnDestroy() {
+			this.Logger.LogInfo("Uninitialized");
+		}
+	}
+}
